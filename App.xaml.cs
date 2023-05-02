9072d0c8deed34baf0ac291c036f5bfc48402d64
josh murphy <jaymurph92@gmail.com>
2023-05-02 19:59:13 -0400

    Finetune starting window size

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,8 +11,14 @@ public partial class App : Application
     protected override Window CreateWindow(IActivationState activationState)
     {
         var window = base.CreateWindow(activationState);
-		window.Height = 750;
+		window.Height = 850;
 		window.Width = 900;
+        // Get display size
+        var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+
+        // Center the window
+        window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2;
+        window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
 		return window;
     }
 }
